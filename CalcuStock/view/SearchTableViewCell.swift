2f7d67a03cd946e8d69819bc119c3c59727b238f
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-10-20 00:15:30 +0700

    ADD TABLEVIEW CELL UI

---
+++ CalcuStock/view/SearchTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SearchTableViewCell.swift
+//  CalcuStock
+//
+//  Created by Gilbert Nicholas on 20/10/21.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+    @IBOutlet weak var assetNameLabel: UILabel!
+    @IBOutlet weak var assetSymbolLabel: UILabel!
+    @IBOutlet weak var assetTypeLabel: UILabel!
+    
+    
+}
