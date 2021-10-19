b1452e036eca07d3bee5f811006e5c8474baf943
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-10-20 01:18:13 +0700

    CONNECT API SERVICES

--- CalcuStock/Service/APIService.swift
+++ CalcuStock/Service/APIService.swift
@@ -6,9 +6,26 @@
 //
 
 import Foundation
+import Combine
 
 struct APIService {
-    let API_KEY1 = "IFFWLCR7SBP9RD9O"
-    let API_KEY2 = "M8ULLHUFLZKVEIV5"
-    let API_KEY3 = "CZ4THDIWEXPV9Q3F"
+    
+    var API_KEY: String {
+        return keys.randomElement() ?? ""
+    }
+    
+    let keys = ["IFFWLCR7SBP9RD9O", "M8ULLHUFLZKVEIV5", "CZ4THDIWEXPV9Q3F"]
+    
+    func fetchSymbolsPublisher(keywords: String) -> AnyPublisher<SearchResults, Error> {
+        
+        let urlString = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(keywords)&apikey=\(API_KEY)"
+        
+        let url = URL(string: urlString)!
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map({ $0.data })
+            .decode(type: SearchResults.self, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
 }
--- CalcuStock/controller/SearchTableViewController.swift
+++ CalcuStock/controller/SearchTableViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class SearchTableViewController: UITableViewController {
     
@@ -19,13 +20,32 @@ class SearchTableViewController: UITableViewController {
         return sc
     }()
     
+    private let apiService = APIService()
+    private var subscribers = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
+        performSearch()
     }
     
     private func setupNavigationBar() {
         navigationItem.searchController = searchController
+    }
+    
+    private func performSearch() {
+        apiService.fetchSymbolsPublisher(keywords: "S&P").sink { (completion) in
+            switch completion {
+            
+            case .failure(let error):
+                print(error.localizedDescription)
+            
+            case .finished: break
+            }
+        } receiveValue: { (searchResults) in
+            print(searchResults)
+        }.store(in: &subscribers)
+
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
