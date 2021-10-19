41a9c2e92caf55bf905c42d94f494ab92cc42a08
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-10-20 00:37:39 +0700

    GETTING API KEY SERVICES

---
+++ CalcuStock/Service/APIService.swift
@@ -0,0 +1,14 @@
+//
+//  APIService.swift
+//  CalcuStock
+//
+//  Created by Gilbert Nicholas on 20/10/21.
+//
+
+import Foundation
+
+struct APIService {
+    let API_KEY1 = "IFFWLCR7SBP9RD9O"
+    let API_KEY2 = "M8ULLHUFLZKVEIV5"
+    let API_KEY3 = "CZ4THDIWEXPV9Q3F"
+}
